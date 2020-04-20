e9a7d4bc6a47e47699a05710ee50d809ce841898
Roy Davis <myroy1024@yahoo.com>
2020-04-20 16:41:22 -0400

    got the create react app set up

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import './App.css';
+
+
+class App extends Component {
+
+
+  render(){
+    return(
+      <h1>Hello World</h1>
+    )
+  }
+}
+
+export default App;
