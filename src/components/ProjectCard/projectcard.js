5c7d4bb15d340df828f17875bfbac2d9e553e350
Roy Davis <myroy1024@yahoo.com>
2020-04-27 12:44:33 -0400

    added my projects to the page

--- src/components/ProjectCard/projectcard.js
+++ src/components/ProjectCard/projectcard.js
@@ -2,15 +2,17 @@ import React from "react";
 
 const ProjectCard = (props) => {
     return(
-        <div className="card">
+        <div className="card horizontal">
             <div className="card-image">
             <img className="project-picture" src={props.image} />
             </div>
+            <div className="card-stacked">
             <div className="card-content">
                 <h4>{props.title}</h4>
                 <p>{props.overview}</p>
                 <button className="btn waves-effect waves-light" id="learn" onClick={() => props.gitHub()}>Learn More</button>
                 <button className="btn waves-effect waves-light" id="try" onClick={() => props.tryProject()}>Try Me</button>
+            </div>
             </div>
         </div>
 
--- src/pages/Portfolio/Portfolio.js
+++ src/pages/Portfolio/Portfolio.js
@@ -20,6 +20,40 @@ class Portfolio extends Component {
             title: "Brew Finder",
             overview: "Location finder for closest brewerys to current locale. It uses HTML5, Javascript, CSS3, JQuery, Bootstrap, AJAX, and the Here Maps API as well as the Open Brewery DB API. The Guardian API was also used for the carousel.",
             image: "./images/beer.jpg"
+        },
+        {
+            id: 2,
+            title: "Cash Money",
+            overview: "Full-stack application where users can create a budget, track spending, and plan for upcoming bills. To start, the user will only be allowed access to the Register or Login pages. Upon registering as a new user or logging in as an existing user, they will be able to access all of the application's pages. They will also have a personalized greeting with their first name printed to the navbar for all subsequent pages. This application uses HTML5, CSS3, Javascript, Bootstrap4, React.js, MongoDB, Mongoose, Express.js, Node.js, Axios, React Chartjs2, and FullCalendar.",
+            image: "./images/money.jpg"
+        },
+
+        {
+            id: 3,
+            title: "unWrapped",
+            overview: "A party planning web application that allows users to create and rsvp to parties while also keeping track of guests, gifts and if they sent a thank you note. This application uses HTML5, CSS3, Javascript, Bootstrap4, JQuery, MySql, Sequalize, Express.js, Node.js, Express-handlebars and Randomatic.",
+            image: "./images/gift.jpg"
+        },
+
+        {
+            id: 4,
+            title: "Friend Finder",
+            overview: "To use this app, a user must answer 10 survey questions. The answers range from 1 (Strongly Disagree) and go to 5 (Strongly Agree). Based on the responses the user gives, the app will match them with someone that most closely fits with the answers they gave. This application uses HTML5, CSS3, Javascript, JQuery, Bootstrap4, and Express.js.",
+            image: "./images/love.jpg"
+        },
+
+        {
+            id: 5,
+            title: "Scraped",
+            overview: "An app that scrapes the technology section of the New York Times. It uses HTML5, Javascript, CSS3, Materialize, JQuery, Mongoose, Mongo DB, Handlebars.js, Express.js and Node.js.",
+            image: "./images/paper.jpg"
+        },
+
+        {
+            id: 6,
+            title: "Bamazon",
+            overview: "This app allows users to preview items for sale or purchase an item from the list of available items that are stored in a mysql database we created. There is also a manager function that allows you to keep tabs on the 'business'. This application uses Javascript, Node.js, JQuery and MySql.",
+            image: "./images/bamazon.jpg"
         }
     ]
     setProjects = () => {
@@ -51,7 +85,7 @@ class Portfolio extends Component {
         return(
             <div className="container">
                 <div className="row">
-                    <div className="col s4">
+                    <div className="col s12">
                         {this.myProjects}}
                     </div>
                 </div>
