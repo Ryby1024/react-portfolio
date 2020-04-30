47d8532543aad4aa393c5611a125fc718a624042
Roy Davis <myroy1024@yahoo.com>
2020-04-30 15:58:23 -0400

    Updates

---
+++ precache-manifest.1d5cd6e50d6ae786a30d9fd4f0f5771c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02c14c7243c51d4af2b89a7e0acbba41",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "4dafda816954b292e830",
+    "url": "/react-portfolio/static/css/main.7143cfde.chunk.css"
+  },
+  {
+    "revision": "6f53cd81f47dd03fb7d3",
+    "url": "/react-portfolio/static/js/2.d88f2d6c.chunk.js"
+  },
+  {
+    "revision": "890b83c3b503af9a5630afd9ff112890",
+    "url": "/react-portfolio/static/js/2.d88f2d6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4dafda816954b292e830",
+    "url": "/react-portfolio/static/js/main.05a536df.chunk.js"
+  },
+  {
+    "revision": "eb3338109e8ace3896f4",
+    "url": "/react-portfolio/static/js/runtime-main.fc5d8309.js"
+  }
+]);
