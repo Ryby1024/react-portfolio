166ed6ca1b0efc977d6d56499b2178c1e4b44898
Roy Davis <myroy1024@yahoo.com>
2021-09-24 16:45:29 -0400

    Updates

---
+++ precache-manifest.8c2531a7f393036a7e9edaa53e9fb50c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "401e9cb8f255299a1b0b8c0e7f2646f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d72aeb7776b33a22c57",
+    "url": "/static/css/main.d1a29bc7.chunk.css"
+  },
+  {
+    "revision": "24d834174b2babeef6e9",
+    "url": "/static/js/2.cac2dd50.chunk.js"
+  },
+  {
+    "revision": "890b83c3b503af9a5630afd9ff112890",
+    "url": "/static/js/2.cac2dd50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d72aeb7776b33a22c57",
+    "url": "/static/js/main.7c364205.chunk.js"
+  },
+  {
+    "revision": "605d82f25e85785a6aaf",
+    "url": "/static/js/runtime-main.ba9d4b64.js"
+  }
+]);
